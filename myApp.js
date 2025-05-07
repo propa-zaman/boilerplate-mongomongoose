21c4e48e544e2f3adff6d75b3a60893ac2ddaf7b
afrazaman <afra@nagorik.tech>
2025-05-07 17:25:36 +0600

    mongooes connected to a database

--- myApp.js
+++ myApp.js
@@ -1,4 +1,17 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB Atlas!');
+})
+.catch(err => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
 
 
 let Person;
